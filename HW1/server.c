521e86ac95339bc029c2383b76794cc708ccf6ca
Yehia_Qtaish <yehiaqtaish@gmail.com>
2020-10-17 18:57:09 -0700

    Pranav and I worked together for almost 3 hours and researched privilege seperation to get it done

--- HW1/server.c
+++ HW1/server.c
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <netinet/in.h>
 #include <string.h>
+#include <pwd.h>
 #define PORT 8080
 int main(int argc, char const *argv[])
 {
@@ -14,8 +15,16 @@ int main(int argc, char const *argv[])
     int addrlen = sizeof(address);
     char buffer[102] = {0};
     char *hello = "Hello from server";
+    // Dev.to Struct (reference to pwnam usage) https://dev.to/0xbf/use-getpwname-to-get-uid-by-login-name-c-lang-4bc2
+    char name[30] = {"nobody"};
+    struct passwd* pwd;
 
     printf("execve=0x%p\n", execve);
+
+    pwd = getpwnam(name);
+    if(pwd == NULL) {
+       printf("pwd empty");   
+    }
 
     // Creating socket file descriptor
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
@@ -31,9 +40,12 @@ int main(int argc, char const *argv[])
         perror("setsockopt");
         exit(EXIT_FAILURE);
     }
+
+
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons( PORT );
+
 
     // Forcefully attaching socket to the port 8080
     if (bind(server_fd, (struct sockaddr *)&address,
@@ -42,20 +54,50 @@ int main(int argc, char const *argv[])
         perror("bind failed");
         exit(EXIT_FAILURE);
     }
-    if (listen(server_fd, 3) < 0)
-    {
-        perror("listen");
-        exit(EXIT_FAILURE);
+    // This is when the application listens to the socket (processes data)
+    // After the socket is created then is attached to the 8080 port, we let socket address struct be set and then 
+    // once the bind is complete we know the root process is attached to the port
+    // At this point we know that the the root privileges can be dropped by
+    // forking the root process and then dropping privileges of the child to the nobody user
+    // since the connection is already established the read and write data processing using the new_socket
+    // can be done with droppped privileges
+
+    // We store the forked process (child) into pid_t variable
+    pid_t child = fork();
+    int returnstatus;
+    // first if the forked value is larger than 0 means we are in the parent process
+    if(child > 0) {
+        // Call waitpid to wait for the child process to exit
+        while(waitpid(child, &returnstatus,0)>0);
+        // exit parent
+        exit(0); 
     }
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
-                       (socklen_t*)&addrlen))<0)
-    {
-        perror("accept");
-        exit(EXIT_FAILURE);
+    else if (child == 0) {
+        // print statement to show the nobody name using pwnam 
+        printf("pwd name = %d \n",pwd->pw_name);
+        // Here change child priveleges to the pw_name we received from pwnam (drop priviliges)
+        setuid(pwd->pw_name);
+        // now server will listen through the nobody process (data processing)
+        if (listen(server_fd, 3) < 0)
+        {
+            perror("listen");
+            exit(EXIT_FAILURE);
+        }
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
+            (socklen_t*)&addrlen))<0)
+        {
+            perror("accept");
+            exit(EXIT_FAILURE);
+        }
+        valread = read( new_socket , buffer, 1024);
+        printf("%s\n",buffer );
+        send(new_socket , hello , strlen(hello) , 0 );
+        printf("Hello message sent\n");
+        // After child is done data processing it will exit the child process
+        exit(0);
+    } else {
+        printf("ERROR");
+        exit(child);
     }
-    valread = read( new_socket , buffer, 1024);
-    printf("%s\n",buffer );
-    send(new_socket , hello , strlen(hello) , 0 );
-    printf("Hello message sent\n");
     return 0;
 }
